5f48662ce549646053b1d756fb817b8207736d99
Andreya <avalabek@yahoo.com>
2018-01-24 12:02:43 -0700

    build letter constructor

--- letter.js
+++ letter.js
@@ -1,16 +1,19 @@
 var Word = require("./letter.js");
 //constructor to build user guess
-var Letter = function(userGuess){
-    this.userGuess = userGuess;
+var Letter = function(value){
+    this.value = value;
     this.show = false;
 }
 //put function on prototype to show correct guess
 Letter.prototype.reveal = function(){
-    if (this.show === true){
-         console.log(this.userGuess);
-        //need to reassign this.show to true after each guess?? or change it so that this.userGuess===the right letter    }else{
-        console.log("_");
-    } 
+    if (this.show === false){
+         return "_";
+    }else{
+     return value;
     }
+        //need to reassign this.show to true after each guess?? or change it so that this.userGuess===the right letter    }else{
+        
+    } 
+    
 
 module.exports = Letter;
