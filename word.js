0cd76bb4ded84676c39776fc8e049eb839a34efd
Andreya <avalabek@yahoo.com>
2018-01-24 12:59:05 -0700

    build word constructor and prototype

--- word.js
+++ word.js
@@ -1,34 +1,67 @@
 var Letter = require("./letter.js");
 
-var Word = function(userGuess){
-    this.userGuess = userGuess;
-    this.letters = userGuess{
-        //need to split input at " " and then check letter
+function Word(){
+    this.word = word;
+    this.character = [];
+    this.found = false; 
+
+           
+
+Word.prototype.getLetters = function(){
+    for (var i=0; i < this.word.length; i++){
+        var newLetter = new Letter(this.word[i]);
+        this.character.push(newLetter);
     }
-
 }
 
-showWord(){
-    var word = this.letters
-//join them by ""
+//treat word like an array take the index and check against it looop
+Word.prototype.guess = function(guessLetter){
+    var score = 0;
+    for (var i = 0; i < this.character.length; i++){
+        if (this.character[i].character===guessLetter){
+            this.character[i].show = true;
+            ++score;
+            console.log("Number of guesses: " + score);
+            return score;
+        }
+    }
 }
-console.log(word);
 
+//translate the below
+//         .map(l => {
+//             var match = (guess === l.value);
+//             l.visible = l.visible || match;
+//             return match;
+//         })
+//         // return true if any new letter was matched
+//         .some(v => v);
+// }
 
-userGuess(guess){
-    this.letters.map (function(letter){
-        if (guess === letter.userGuess){
-            letter.show = true;
+// should all these functions be on prototype or?
+Word.prototype.findLetter = function(){
+    var found = false;
+    if (this.character.every(isTrue()){
+        found = true;
+        return found;
+    }
+function isTrue(value, index,ar){
+//not sure about return statements or .every
+    
+        if (value.show === true) {
             return true;
-        }else {
+        } else {
             return false;
         }
-        
-    })
-    
+    }
 }
-gameOver(){
-    return this.showWord() === this.userGuess;
+Word.prototype.revealWord = function(){
+    var str = "";
+    for (var i=0; i< this.character.length; i++){
+        str += this.character[i].reveal();
+        console.log("test" + this.character[i]);
+        console.log("reveal word: " + str);
+        return str;
+    }
 }
 }
 module.exports = Word;
